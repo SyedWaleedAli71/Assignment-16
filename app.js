3da1df431ebf075912a6429981f37cdf1d70a2fd
SyedWaleedAli71 <syedwaleed887@gmail.com>
2025-11-28 23:19:48 +0500

    first-commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// Assignment # 16 
+// ARRAYS AND LOOP 
+// Q.1
+// var multiArray = [,[],[]];
+// 
+// Q.2
+// var matrix = [[0,1,2,3],[1,0,1,2],[2,1,0,1]];
+// console.log(matrix);
+// Q.3
+// for (var i = 1; i <= 10; i++){
+//     document.writeln(i + "<br>");
+// };
+// 
+// Q.4
+// var tableNumber = +prompt("Enter Table Number..");
+// var tableLength = +prompt("Enter Table Length");
+
+// for (var i = 1; i <= tableLength; i++){
+//     document.writeln(tableNumber + " x " + i + " = " + (tableNumber * i) + "<br>")
+// }
+// 
+// Q.5
+
